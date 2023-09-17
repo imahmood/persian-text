71287d3eef5791f29e05820ff6fa018ad4556d87
Mahmood Dehghani <mahmood.public@gmail.com>
2023-09-17 10:56:48 +0330

    improved type hinting

--- src/Localized/PersianText.php
+++ src/Localized/PersianText.php
@@ -1,132 +1,161 @@
 <?php
-namespace imahmood\Localized;
+declare(strict_types=1);
+
+namespace Imahmood\Localized;
 
 use InvalidArgumentException;
 use NumberFormatter;
+use RuntimeException;
 
 class PersianText
 {
     /**
-     * @var string[]
+     * @var array<int, string>
      */
-    protected static $enNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    protected static array $enNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     /**
-     * @var string[]
+     * @var array<int, string>
      */
-    protected static $arNumbers = ['۰', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+    protected static array $arNumbers = ['۰', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
 
     /**
-     * @var string[]
+     * @var array<int, string>
      */
-    protected static $faNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+    protected static array $faNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
 
     /**
-     * @var string[]
+     * @var array<int, string>
      */
-    protected static $arChars = ['ي', 'ى', 'ك', 'ة'];
+    protected static array $arChars = ['ي', 'ى', 'ك', 'ة'];
 
     /**
-     * @var string[]
+     * @var array<int, string>
      */
-    protected static $faChars = ['ی', 'ی', 'ک', 'ه'];
+    protected static array $faChars = ['ی', 'ی', 'ک', 'ه'];
 
     /**
-     * @param string|int|float $string The string being formatted.
-     * @return string|int|float
+     * Transforms a string to Persian.
      */
-    public static function text($string)
+    public static function text(float|int|string|null $string): ?string
     {
         $string = static::onlyArabicChars($string);
         return static::onlyNumbers($string);
     }
 
     /**
-     * @param string|int|float $string The string being formatted.
-     * @return string|int|float
+     * Replaces Arabic characters and numbers with Persian equivalents.
      */
-    public static function onlyArabicChars($string)
+    public static function onlyArabicChars(float|int|string|null $string): ?string
     {
         $string = static::convert(self::$arChars, self::$faChars, $string);
         return static::convert(self::$arNumbers, self::$faNumbers, $string);
     }
 
     /**
-     * @param string|int|float $string The string being formatted.
-     * @return string|int|float
+     * Replaces English and Arabic numbers with Persian numbers.
      */
-    public static function onlyNumbers($string)
+    public static function onlyNumbers(float|int|string|null $string): ?string
     {
         $string = static::convert(self::$enNumbers, self::$faNumbers, $string);
         return static::convert(self::$arNumbers, self::$faNumbers, $string);
     }
 
     /**
-     * @param string|int|float $string The string being formatted.
-     * @return string|int|float
+     * Convert a string containing Persian or Arabic numbers to English numbers.
      */
-    public static function toEnglishNumber($string)
+    public static function toEnglishNumber(float|int|string|null $string): ?string
     {
         $string = static::convert(self::$faNumbers, self::$enNumbers, $string);
         return static::convert(self::$arNumbers, self::$enNumbers, $string);
     }
 
     /**
-     * @param mixed $number Number
+     * Formats a number as a money string.
+     *
+     * @param mixed $number The number to format
      * @param int $decimals Decimal points
      * @param string $decimalPoint Separator for the decimal point
      * @param string $thousandsSep Thousands separator
-     * @return string
+     * @return string|null
      */
-    public static function money($number, $decimals = 0, $decimalPoint = '٫', $thousandsSep = '٬')
-    {
-        return number_format(static::parseValue($number), $decimals, $decimalPoint, $thousandsSep);
+    public static function money(
+        mixed $number,
+        int $decimals = 0,
+        string $decimalPoint = '٫',
+        string $thousandsSep = '٬',
+    ): ?string {
+        if ($number === null) {
+            return null;
+        }
+
+        $formattedNumber = number_format(static::parseFloat($number), $decimals, $decimalPoint, $thousandsSep);
+
+        return static::onlyNumbers($formattedNumber);
     }
 
     /**
-     * @param mixed $number Number
-     * @param string $locale Locale
-     * @return string
+     * Convert a numeric value to its spelled-out representation.
+     *
+     * @param mixed $number The numeric value to be converted.
+     * @param string $locale The locale to use for formatting (default is 'fa_IR' for Persian).
+     * @return string|null
      */
-    public static function asSpellout($number, $locale = 'fa_IR')
+    public static function asSpellout(mixed $number, string $locale = 'fa_IR'): ?string
     {
+        if ($number === null) {
+            return null;
+        }
+
         $formatter = new NumberFormatter($locale, NumberFormatter::SPELLOUT);
+        $formattedNumber = $formatter->format(static::parseFloat($number));
 
-        return $formatter->format(static::parseValue($number));
+        if ($formattedNumber === false) {
+            throw new RuntimeException('Failed to format the number as spellout.');
+        }
+
+        return $formattedNumber;
     }
 
     /**
-     * @param mixed $value Value
-     * @return float
+     * Parse a mixed value to a floating-point number.
      */
-    protected static function parseValue($value)
+    protected static function parseFloat(mixed $number): float
     {
-        if (is_object($value)) {
-            if (method_exists($value, 'toFloat')) {
-                return $value->toFloat();
+        if (is_object($number)) {
+            if (method_exists($number, 'toFloat')) {
+                return $number->toFloat();
             }
 
-            if (method_exists($value, '__toString')) {
-                $value = strval($value);
+            if (method_exists($number, '__toString')) {
+                $number = (string)$number;
             }
         }
 
-        if (!is_numeric($value)) {
-            throw new InvalidArgumentException(sprintf('"%s" is not numeric.', $value));
+        if (!is_numeric($number)) {
+            throw new InvalidArgumentException('$value is not numeric.');
         }
 
-        return (float)$value;
+        return (float)$number;
     }
 
     /**
-     * @param array $search
-     * @param array $replace
-     * @param mixed $subject
-     * @return mixed
+     * Replaces elements in a string based on search and replace arrays.
+     *
+     * @param array<int, string> $search
+     * @param array<int, string> $replace
+     * @param float|int|string|null $subject
+     * @return string|null
      */
-    protected static function convert(array $search, array $replace, $subject)
+    protected static function convert(array $search, array $replace, float|int|string|null $subject): ?string
     {
-        if (trim($subject) === '' || is_bool($subject)) {
+        if ($subject === null) {
+            return null;
+        }
+
+        $subject = (string)$subject;
+
+        if (trim($subject) === '') {
             return $subject;
         }
 
